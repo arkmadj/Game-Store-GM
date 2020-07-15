f48a8c630bb30363881985eea9b437a3a96142b3
ahmadjinadu <ahmadjinadu@gmail.com>
2020-07-15 12:23:55 +0100

    heroku commit

--- config/database.js
+++ config/database.js
@@ -87,8 +87,8 @@ module.exports = {
   pg: {
     client: 'pg',
     connection: {
-      host: Env.get('DB_HOST', DATABASE_URL.host),
-      port: Env.get('DB_PORT',''),
+      host: Env.get('DB_HOST', DATABASE_URL.hostname),
+      port: Env.get('DB_PORT', DATABASE_URL.port),
       user: Env.get('DB_USER', DATABASE_URL.username),
       password: Env.get('DB_PASSWORD', DATABASE_URL.password),
       database: Env.get('DB_DATABASE', DATABASE_URL.pathname.substr(1))
